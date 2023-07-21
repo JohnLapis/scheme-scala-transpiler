543943ebd834e4db54d4db994a15e4c53007914e
john <borges-2014@live.com>
2023-07-21 05:38:34 +0000

    test: Add test for parsing procedure

--- src/test/java/transpiler/scheme/SchemeParserTest.java
+++ src/test/java/transpiler/scheme/SchemeParserTest.java
@@ -56,10 +56,80 @@ public class SchemeParserTest
         compareASTNodes(expectedAst, parser.parse());
     }
 
-    @Ignore("TODO")
     @Test
     public void parseProcedures()
     {
+        String code = """
+(define fact
+  (lambda (n)
+    (if (= n 0)
+        1
+      (* n (fact (- n 1))))))
+            """;
+        ASTNode expectedAst =
+            n("PROGRAM",
+              n("COMMAND_OR_DEFINITION",
+                n("DEFINITION", "define",
+                  n("IDENTIFIER", "fact"),
+                  n("EXPRESSION",
+                    n("LAMBDA_EXPRESSION", "lambda",
+                      n("FORMALS",
+                        n("VARIABLE",
+                          n("IDENTIFIER", "n"))),
+                      n("BODY",
+                        n("SEQUENCE",
+                          n("EXPRESSION",
+                            n("CONDITIONAL", "if",
+                              n("TEST",
+                                n("EXPRESSION",
+                                  n("PROCEDURE_CALL",
+                                    n("OPERATOR",
+                                      n("EXPRESSION",
+                                        n("IDENTIFIER", "="))),
+                                    n("OPERAND",
+                                      n("EXPRESSION",
+                                        n("IDENTIFIER", "n"))),
+                                    n("OPERAND",
+                                      n("EXPRESSION",
+                                        n("LITERAL",
+                                          n("SELF_EVALUATING",
+                                            n("NUMBER", "0")))))))),
+                              n("CONSEQUENT",
+                                n("EXPRESSION",
+                                  n("LITERAL",
+                                    n("SELF_EVALUATING",
+                                      n("NUMBER", "1"))))),
+                              n("ALTERNATE",
+                                n("EXPRESSION",
+                                  n("PROCEDURE_CALL",
+                                    n("OPERATOR",
+                                      n("EXPRESSION",
+                                        n("IDENTIFIER", "*"))),
+                                    n("OPERAND",
+                                      n("EXPRESSION",
+                                        n("IDENTIFIER", "n"))),
+                                    n("OPERAND",
+                                      n("EXPRESSION",
+                                        n("PROCEDURE_CALL",
+                                          n("OPERATOR",
+                                            n("EXPRESSION",
+                                              n("IDENTIFIER", "fact"))),
+                                          n("OPERAND",
+                                            n("EXPRESSION",
+                                              n("PROCEDURE_CALL",
+                                                n("OPERATOR",
+                                                  n("EXPRESSION",
+                                                    n("IDENTIFIER", "-"))),
+                                                n("OPERAND",
+                                                  n("EXPRESSION",
+                                                    n("IDENTIFIER", "n"))),
+                                                n("OPERAND",
+                                                  n("EXPRESSION",
+                                                    n("LITERAL",
+                                                      n("SELF_EVALUATING",
+                                                        n("NUMBER", "1")))))))))))))))))))))));
+        SchemeParser parser = new SchemeParser(SchemeScanner.tokenize(code));
+        compareASTNodes(expectedAst, parser.parse());
     }
 
     @Test
