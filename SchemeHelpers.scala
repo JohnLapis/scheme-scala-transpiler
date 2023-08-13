6e1cf5afff175491194e099799ec52457ea90a72
john <borges-2014@live.com>
2023-08-13 23:21:37 +0000

    feat[unparser]: Implement more procedures for comparing numbers

--- SchemeHelpers.scala
+++ SchemeHelpers.scala
@@ -1,8 +1,15 @@
 package object SchemeHelpers {
-  def numEquals(nums: Number*): Boolean = _numEquals(nums)
-  private def _numEquals(nums: Seq[Number]): Boolean =
-  if nums.length <= 1 then
-    true
-  else
-    nums(0) == nums(1) && _numEquals(nums.drop(1))
+  def numsEq(nums: Number*): Boolean = compareNumbers(nums, _ == _)
+  // def numsLt(nums: Number*): Boolean = compareNumbers(nums, _ < _)
+  // def numsLe(nums: Number*): Boolean = compareNumbers(nums, _ <= _)
+  // def numsGt(nums: Number*): Boolean = compareNumbers(nums, _ > _)
+  // def numsGe(nums: Number*): Boolean = compareNumbers(nums, _ >= _)
+  private def compareNumbers(
+    nums: Seq[Number],
+    operator: (Number, Number) => Boolean
+  ): Boolean =
+    if nums.length <= 1 then
+      true
+    else
+      operator(nums(0), nums(1)) && compareNumbers(nums.drop(1), operator)
 }
